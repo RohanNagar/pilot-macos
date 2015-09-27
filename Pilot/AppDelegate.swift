0a071a2254e5daf8e609740a628f14a2f93b1187
Rohan Nagar <rohan.nagar@utexas.edu>
2015-09-26 20:16:39 -0500

    Set up XCode project

---
+++ Pilot/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pilot
+//
+//  Created by Rohan Nagar on 9/26/15.
+//  Copyright © 2015 Sanction. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
