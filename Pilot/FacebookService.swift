d8f821fbc866a20a53e6273668fba1bfa9a23ef1
Rohan Nagar <rohan.nagar@utexas.edu>
2015-10-09 15:43:13 -0500

    Use headers for authorization instead of a challenge response

--- Pilot/FacebookService.swift
+++ Pilot/FacebookService.swift
@@ -10,13 +10,23 @@ import Cocoa
 import Alamofire
 
 class FacebookService: NSObject {
-  // Credentials for making requests to Lightning
-  let lightningCredential = NSURLCredential(user: "lightning",
-                                        password: "secret",
-                                     persistence: .ForSession)
+  // Headers to use on HTTP requests to Lightning
+  var headers: [String:String]
+
   // Endpoints to use
   let photosEndpoint = "http://localhost:9000/facebook/photos"
   let videosEndpoint = "http://localhost:9000/facebook/videos"
+  
+  /* Default init */
+  override init() {
+    let user = "lightning"
+    let password = "secret"
+    
+    let credentialData = "\(user):\(password)".dataUsingEncoding(NSUTF8StringEncoding)!
+    let base64Credentials = credentialData.base64EncodedStringWithOptions([])
+    
+    headers = ["Authorization": "Basic \(base64Credentials)"]
+  }
   
   /*
    * Sends a request to Lightning for all Facebook photo URLs the user has.
@@ -26,15 +36,14 @@ class FacebookService: NSObject {
    *    completion: The method to call upon completion. Will pass in the array of URLs to the method.
    */
   func getFacebookPhotoUrls(username: String, completion: (([String]) -> Void)) {
-    Alamofire.request(.GET, photosEndpoint, parameters: ["username": username])
-      .authenticate(usingCredential: lightningCredential)
+    Alamofire.request(.GET, photosEndpoint, headers: headers, parameters: ["username": username])
       .responseJSON { response in
         var urls = [String]()
         
         // Iterate through JSON response, adding URLs to array
         if let JSON = response.result.value {
           for var i = 0; i < JSON.count; i++ {
-            urls.append(JSON[i]["uri"] as! String)
+            urls.append(JSON[i]["url"] as! String)
           }
         }
         
@@ -51,8 +60,7 @@ class FacebookService: NSObject {
    *    completion: The method to call upon completion. Will pass in the array of URLs to the method.
    */
   func getFacebookVideoUrls(username: String, completion: (([String]) -> Void)) {
-    Alamofire.request(.GET, videosEndpoint, parameters: ["username": username])
-      .authenticate(usingCredential: lightningCredential)
+    Alamofire.request(.GET, videosEndpoint, headers: headers, parameters: ["username": username])
       .responseJSON { response in
         var urls = [String]()
         
