5de1f99750e6cd49ffcceb1582f0509da128106a
Rohan Nagar <rohan.nagar@utexas.edu>
2015-11-03 13:54:51 -0600

    Integrate Tailor into workflow

--- PilotTests/PasswordServiceTest.swift
+++ PilotTests/PasswordServiceTest.swift
@@ -16,24 +16,24 @@ class PasswordServiceTest: XCTestCase {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
-    
+
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
-  
+
   /* PasswordHash Tests */
   func testHashSamePassword() {
     let firstHash = PasswordService.hashPassword("Password")
     let secondHash = PasswordService.hashPassword("Password")
-    
+
     expect(firstHash).to(equal(secondHash))
   }
-  
+
   func testHashDifferentPassword() {
     let firstHash = PasswordService.hashPassword("FirstPassword")
     let secondHash = PasswordService.hashPassword("SecondPassword")
-    
+
     expect(firstHash).toNot(equal(secondHash))
   }
 
